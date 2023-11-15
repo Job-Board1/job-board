9358b3a2e86c29d8b1dadf18bf9f43bcdd2f17e4
khaoulasellami3 <khaoulasellami37@gmail.com>
2023-11-15 19:19:07 +0100

    khawla

--- client/src/App.js
+++ client/src/App.js
@@ -2,3 +2,4 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
+// aa
